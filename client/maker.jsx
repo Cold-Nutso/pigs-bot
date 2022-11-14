25413279df83311fcb596fd9f07687ca6afa8d39
Cold-Nutso <vwp5337@rit.edu>
2022-11-13 22:02:11 -0500

    Ported over domoMaker.

---
+++ client/maker.jsx
@@ -0,0 +1,136 @@
+const helper = require('./helper.js');
+
+const loadDomosFromServer = async () => {
+    const response = await fetch('/getDomos');
+    const data = await response.json();
+    ReactDOM.render(
+        <DomoList domos={data.domos} />,
+        document.getElementById('domos')
+    );
+};
+
+const handleDomo = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const name = e.target.querySelector('#domoName').value;
+    const age = e.target.querySelector('#domoAge').value;
+    const food = e.target.querySelector('#domoFood').value;
+    const _csrf = e.target.querySelector('#_csrf').value;
+
+    if (!name && age && food) {
+        helper.handleError('Name field is required!');
+        return false;
+    } else if (name && !age && food) {
+        helper.handleError('Age field is required!');
+        return false;
+    } else if (name && age && !food) {
+        helper.handleError('Food field is required!');
+        return false;
+    }
+    else if (!name || !age || !food) {
+        helper.handleError('All fields are required!');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, {name, age, food, _csrf}, loadDomosFromServer);
+
+    return false;
+};
+
+const handleDeleteDomo = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const domoDisplay = e.target.parentNode;
+    const _id = domoDisplay.dataset.key;
+    const _csrf = document.querySelector('#_csrf').value;
+
+    if (!_id) {
+        helper.handleError("Couldn't find the key.");
+        return false;
+    }
+
+    helper.sendPost(e.target.action, { _id, _csrf }, loadDomosFromServer);
+
+    // This is redundant, but it just wouldn't update properly otherwise
+    loadDomosFromServer();
+
+    return false;
+};
+
+const DomoForm = (props) => {
+    return (
+        <form id="domoForm"
+            onSubmit={handleDomo}
+            name="domoForm"
+            action="/maker"
+            method="POST"
+            className="domoForm"
+        >
+            <label htmlFor="name">Name: </label>
+            <input id="domoName" type="text" name="name" placeholder="Domo" />
+            <label htmlFor="age">Age: </label>
+            <input id="domoAge" type="number" min="0" name="age" placeholder="20"/>
+            <label htmlFor="food">Favorite Food: </label>
+            <input id="domoFood" type="text" name="food" placeholder="nikujaga"/>
+            <input id="_csrf" type="hidden" name="_csrf" value={props.csrf} />
+            <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+        </form>
+    );
+};
+
+const DomoList = (props) => {
+    if (props.domos.length === 0) {
+        return (
+            <div className="domoList">
+                <h3 className="emptyDomo">No Domos Yet!</h3>
+            </div>
+        );
+    }
+
+    const domoNodes = props.domos.map(domo => {
+        return (
+            <div key={domo._id} data-key={domo._id} className="domo">
+                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+                <h3 className="domoName"> Name: {domo.name} </h3>
+                <h3 className="domoAge"> Age: {domo.age} </h3>
+                <h3 className="domoFood"> Favorite Food: {domo.food} </h3>
+                <form
+                    onSubmit={handleDeleteDomo}
+                    name="deleteForm"
+                    action="/delete"
+                    method="POST"
+                    className="deleteForm"
+                >
+                    <input className="deleteDomoSubmit" type="submit" value="DELETE" />
+                </form>
+            </div>
+        );
+    });
+
+    return (
+        <div className="domoList">
+            {domoNodes}
+        </div>
+    );
+};
+
+const init = async () => {
+    const response = await fetch('/getToken');
+    const data = await response.json();
+
+    ReactDOM.render(
+        <DomoForm csrf={data.csrfToken} />,
+        document.getElementById('makeDomo')
+    );
+
+    ReactDOM.render(
+        <DomoList domos = {[]} />,
+        document.getElementById('domos')
+    );
+
+    loadDomosFromServer();
+};
+
+window.onload = init;
---
+++ server/controllers/Domo.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+// Create a model just to delete domos
+mongoose.model('Deletor', new mongoose.Schema({}), 'domos');
+const deletor = mongoose.model('Deletor');
+
+const models = require('../models');
+
+const { Domo } = models;
+
+const makerPage = (req, res) => res.render('app');
+
+const makeDomo = async (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.food) {
+    return res.status(400).json({ error: 'All fields are required!' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    food: req.body.food,
+    owner: req.session.account._id,
+  };
+
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age, food: newDomo.food });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists!' });
+    }
+    return res.status(400).json({ error: 'An error occurred' });
+  }
+};
+
+const deleteDomo = async (req, res) => {
+  if (!req.body._id) {
+    return res.status(400).json({ error: 'Missing domo id.' });
+  }
+
+  try {
+    await deletor.deleteOne({ _id: mongoose.Types.ObjectId(req.body._id) });
+
+    return res.status(200).json({ message: 'Domo successfully deleted.' });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: 'An error occurred' });
+  }
+};
+
+const getDomos = (req, res) => {
+  Domo.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error has occurred!' });
+    }
+
+    return res.json({ domos: docs });
+  });
+};
+
+module.exports = {
+  makerPage,
+  makeDomo,
+  deleteDomo,
+  getDomos,
+};
